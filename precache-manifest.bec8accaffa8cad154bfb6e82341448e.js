085358d91e718a8e8c204eadb013bf9bdaa786c5
aashishlm <giriaashish.lm10@gmail.com>
2021-06-25 19:24:22 +0545

    Updates

---
+++ precache-manifest.bec8accaffa8cad154bfb6e82341448e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b535d82bde64670f79d327f1870302",
+    "url": "/netflixClone/index.html"
+  },
+  {
+    "revision": "30f219befcb68222a09c",
+    "url": "/netflixClone/static/css/main.35f09b59.chunk.css"
+  },
+  {
+    "revision": "a60630449c1a0c4c2c97",
+    "url": "/netflixClone/static/js/2.c38e2562.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflixClone/static/js/2.c38e2562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30f219befcb68222a09c",
+    "url": "/netflixClone/static/js/main.9927dd5d.chunk.js"
+  },
+  {
+    "revision": "c2ebfca4b867fee391cd",
+    "url": "/netflixClone/static/js/runtime-main.003b2803.js"
+  }
+]);
